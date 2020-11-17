2562496bcdc97b8ec24c204de7ea9a8006958ea3
mezzzi <mezzzi92@gmail.com>
2020-11-17 21:57:51 +0300

    simulation modes added

--- src/components/vm-translator/execution-simulator/hooks/useGeneralReducer.js
+++ src/components/vm-translator/execution-simulator/hooks/useGeneralReducer.js
@@ -6,6 +6,11 @@ const ACTIONS = {
   SET_GLOBAL_STACK: 'globalStack',
   SET_TRANSLATOR: 'translator',
   SET_SIMULATION_MODE_ON: 'isSimulationModeOn',
+  SET_HVM_SIMULATION_ON: 'isHvmSimulationOn',
+  SET_ASM_SIMULATION_ON: 'isAsmSimulationOn',
+  SET_ARITHMETIC_SIMULATION_ON: 'isArithmeticSimulationOn',
+  SET_PUSH_SIMULATION_ON: 'isPushSimulationOn',
+  SET_POP_SIMULATION_ON: 'isPopSimulationOn',
   SET_IS_SIMULATING: 'isSimulating'
 }
 
@@ -24,6 +29,11 @@ const useGeneralReducer = () => {
     globalStack: [],
     translator: null,
     isSimulationModeOn: true,
+    isHvmSimulationOn: true,
+    isAsmSimulationOn: true,
+    isArithmeticSimulationOn: true,
+    isPushSimulationOn: true,
+    isPopSimulationOn: true,
     isSimulating: false
   })
 
@@ -41,6 +51,11 @@ const useGeneralReducer = () => {
     globalStack: getSetter('SET_GLOBAL_STACK'),
     translator: getSetter('SET_TRANSLATOR'),
     isSimulationModeOn: getSetter('SET_SIMULATION_MODE_ON'),
+    isHvmSimulationOn: getSetter('SET_GLOBAL_STACK'),
+    isAsmSimulationOn: getSetter('SET_TRANSLATOR'),
+    isArithmeticSimulationOn: getSetter('SET_SIMULATION_MODE_ON'),
+    isPushSimulationOn: getSetter('SET_IS_SIMULATING'),
+    isPopSimulationOn: getSetter('SET_IS_SIMULATING'),
     isSimulating: getSetter('SET_IS_SIMULATING')
   }
 
